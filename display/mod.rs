e8bbdcb85e0fec86acfb59294778e05b71c35a34
rayanmorel4498-ai <rayanmorel4498@gmail.com>
2026-02-02 15:04:05 +0100

    Delete display directory

--- display/mod.rs
+++
@@ -1,9 +0,0 @@
-pub mod adaptive_refresh;
-pub mod brightness;
-pub mod display_control;
-pub mod dynamic;
-pub mod framebuffer;
-pub mod screen;
-pub mod stylus;
-pub mod touch;
-pub use screen::{DisplayScreen, TouchScreen, TouchPoint, MAX_TOUCH_POINTS};
