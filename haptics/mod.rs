05b075a78f66afb29009d2c86e7b2008934c18ec
rayanmorel4498-ai <rayanmorel4498@gmail.com>
2026-02-02 15:03:47 +0100

    Delete haptics directory

--- haptics/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod vibrator;
-pub mod linear_actuator;
-pub mod haptics_control;
-pub use haptics_control::HapticsController;
-pub use vibrator::Vibrator;
-pub use linear_actuator::LinearActuator;
