5eb95c6c3994a9d44a4e66c6afbe24060c6f4f3d
rayanmorel4498-ai <rayanmorel4498@gmail.com>
2026-02-02 15:01:37 +0100

    Delete biometric directory

--- biometric/mod.rs
+++
@@ -1,8 +0,0 @@
-pub mod fingerprint;
-pub mod faceid;
-pub mod iris;
-pub mod voice_biometrics;
-pub use fingerprint::Fingerprint;
-pub use faceid::FaceID;
-pub use iris::Iris;
-pub use voice_biometrics::VoiceBiometrics;
