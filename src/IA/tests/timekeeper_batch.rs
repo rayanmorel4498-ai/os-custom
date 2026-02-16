f9966249fccd2da9c753d040a9d8290cb3d41a85
rayanmorel4498-ai <rayanmorel4498@gmail.com>
2026-02-17 00:48:33 +0100

    Delete src/IA/tests directory

--- src/IA/tests/timekeeper_batch.rs
+++
@@ -1,8 +0,0 @@
-use redmi_ia::core::timekeeper::Timekeeper;
-
-#[test]
-fn timekeeper_cached_now() {
-    let tk = Timekeeper::new();
-    let _ = tk.update_time_ms(100);
-    assert_eq!(tk.now_ms_cached(), 100);
-}
