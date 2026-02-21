d77931208314b41d1053c0fe83acc2a03cf19ae7
rayanmorel4498-ai <rayanmorel4498@gmail.com>
2026-02-21 02:44:13 +0100

    Add modules to kernel in mod.rs

---
+++ src/TLS/src/api/kernel/mod.rs
@@ -0,0 +1,10 @@
+pub mod callbacks;
+pub mod mutex;
+pub mod rng;
+pub mod spinlock;
+pub mod task_queue;
+pub mod session_timeout;
+pub mod time_abstraction;
+pub mod hardening;
+pub mod launcher;
+pub mod integration;
