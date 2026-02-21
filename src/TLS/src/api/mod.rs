9fc898d2042e9275730eb0e91ba194e11b6e95ca
rayanmorel4498-ai <rayanmorel4498@gmail.com>
2026-02-21 02:34:30 +0100

    Add files via upload

--- src/TLS/src/api/mod.rs
+++ src/TLS/src/api/mod.rs
@@ -1,26 +1,14 @@
-pub mod config;
-#[path = "config/api.rs"]
-pub mod api;
-#[path = "config/client.rs"]
-pub mod client;
-#[path = "config/client_engine.rs"]
-pub mod client_engine;
-#[path = "config/component_api.rs"]
-pub mod component_api;
-#[path = "config/component_token.rs"]
-pub mod component_token;
-pub mod kernel;
-pub mod hardware;
-pub mod capture_module;
-#[allow(non_snake_case)]
-#[path = "IA/mod.rs"]
-pub mod IA;
-#[path = "config/token.rs"]
-pub mod token;
+pub mod callbacks;
+pub mod hardening;
+pub mod mutex;
+pub mod rng;
+pub mod session_timeout;
+pub mod spinlock;
+pub mod task_queue;
+pub mod time_abstraction;
+pub mod launcher;
+pub mod integration;
 
-pub use client::TLSClient;
-pub use client_engine::TLSClientEngine;
-pub use component_api::*;
-pub use component_token::{ComponentToken, ComponentSignature, ComponentTokenManager, ComponentType};
-pub use config::server::TLSServer;
-pub use token::TokenManager;
+pub use crate::api::config::component_api;
+pub use crate::api::config::component_token;
+pub use crate::api::config::ephemeral_api;
